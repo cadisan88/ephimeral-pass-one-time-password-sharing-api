7248798d3091bf4a8a34abc1aa44849bceeeb9f6
Carlos Diaz <ca.disan88@gmail.com>
2025-03-19 00:48:24 -0400

    Rename health check route to '/up' for clarity

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/health-check', function () {
+// API health check route
+Route::get('/up', function () {
     return response()->json(['status' => 'ok']);
 });
